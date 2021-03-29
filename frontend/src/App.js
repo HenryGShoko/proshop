172b237a97e0ebebe88e995a862cf57090540c82
HenryGShoko <gshokx91@gmail.com>
2021-03-29 15:59:24 +0200

    react setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -9,9 +8,7 @@ const App = () => {
      <>
        <Header />
       <main className='py-3'>
-        <Container>
           <h1>Welcome To ProShop</h1>
-        </Container>
       </main>
       <Footer />
      </>
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
-  return (
-    <footer>
-      <Container>
-        <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
-        </Row>
-      </Container>
-    </footer>
-  )
+    return (
+        <footer>
+            footer
+        </footer>
+    )
 }
 
 export default Footer
