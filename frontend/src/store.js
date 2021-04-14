14fa3eb273753f763fcf3edfe4000fd000354847
HenryGShoko <gshokx91@gmail.com>
2021-04-14 08:44:27 +0200

    Create Order

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,10 +12,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
-
-
-
-
+import { orderCreateReducer } from './reducers/orderReducers'
 
 
 const reducer = combineReducers({
@@ -26,7 +23,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
-
+  orderCreate: orderCreateReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
